010a977f0bf78a94c9ebd8d87b49271a67068f24
jcruz63 <46271776+jcruz63@users.noreply.github.com>
2020-12-28 01:10:16 -0500

    DynamicArray passes inital test cases

--- src/com/turnbasedstudio/datastructs/DynamicArray.java
+++ src/com/turnbasedstudio/datastructs/DynamicArray.java
@@ -1,7 +1,8 @@
 package com.turnbasedstudio.datastructs;
 
 /*
-    This is the answer to https://www.hackerrank.com/challenges/dynamic-array/problem with the corresponding unit test
+    This is the answer to https://www.hackerrank.com/challenges/dynamic-array/problem with the corresponding unit test.
+
  */
 
 
@@ -9,24 +10,24 @@ import java.util.ArrayList;
 
 public class DynamicArray {
 
-    private final ArrayList<ArrayList<Integer>> arrayList;
-    private final int lastAnswer = 0;
+    private final ArrayList<ArrayList<Integer>> masterList;
+    private int lastAnswer = 0;
     private final int numberOfSequences;
 
     public DynamicArray(int numberOfSequences) {
         this.numberOfSequences = numberOfSequences;
-        this.arrayList = new ArrayList<>();
+        this.masterList = new ArrayList<>();
         for (int i = 0; i < numberOfSequences; i++) {
-            arrayList.add(new ArrayList<>());
+            masterList.add(new ArrayList<>());
         }
     }
 
     public int getDynamicArrayLength() {
-        return arrayList.size();
+        return masterList.size();
     }
 
     public void insertIntoSequence(int sequence, int numberToInsert) {
-        arrayList.get(getSequenceListNumber(sequence)).add(numberToInsert);
+        masterList.get(getSequenceListNumber(sequence)).add(numberToInsert);
     }
 
     public int getSequenceListNumber(int sequence) {
@@ -35,4 +36,19 @@ public class DynamicArray {
         }
         return (sequence ^ lastAnswer) % numberOfSequences;
     }
+
+    public int getIntAtSequenceIndex(int sequence, int index) {
+        return masterList.get(getSequenceListNumber(sequence)).get(index);
+    }
+
+    public int query(int queryType, int sequence, int indexOrNumberToInsert) {
+        int ans = 0;
+        switch (queryType) {
+            case 1 -> insertIntoSequence(sequence, indexOrNumberToInsert);
+            case 2 -> ans = getIntAtSequenceIndex(sequence, indexOrNumberToInsert);
+        }
+        lastAnswer = ans;
+        return ans;
+    }
+
 }
--- test/com/turnbasedstudio/datastructs/DynamicArrayTest.java
+++ test/com/turnbasedstudio/datastructs/DynamicArrayTest.java
@@ -20,6 +20,36 @@ class DynamicArrayTest {
 
     @Test
     public void insertIntoSequence() {
+        DynamicArray dynamicArray = new DynamicArray(2);
+        int sequence = 1;
+        int numberToInsert = 5;
+        dynamicArray.insertIntoSequence(sequence, numberToInsert);
+
+        //could have used reflection but as the getter doesn't change the value or logic I am only still testing the ability to insert into the correct list
+        assertEquals(numberToInsert, dynamicArray.getIntAtSequenceIndex(sequence, 0));
+    }
+
+    @Test
+    public void executeQueries() {
+        int numberOfSequences = 2;
+        DynamicArray dynamicArray = new DynamicArray(numberOfSequences);
+        int numberOfQueries = 5;
+        int[][] queries = {
+                {1, 0, 5},
+                {1, 1, 7},
+                {1, 0, 3},
+                {2, 1, 0},
+                {2, 1, 1}
+        };
+        for (int i = 0; i < queries.length; i++) {
+            if (queries[i][0] == 1) {
+                assertEquals(0, dynamicArray.query(queries[i][0], queries[i][1], queries[i][2]));
+            } else if (i == 3) {
+                assertEquals(7, dynamicArray.query(queries[i][0], queries[i][1], queries[i][2]));
+            } else if (i == 4) {
+                assertEquals(3, dynamicArray.query(queries[i][0], queries[i][1], queries[i][2]));
+            }
+        }
     }
 
 }
